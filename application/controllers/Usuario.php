2b16e786494544b07d8ed22b3cc1aabe9a248652
Leonel Mita <102183281+demonik123@users.noreply.github.com>
2022-10-06 10:45:21 -0400

    archivos base

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -11,20 +11,46 @@ class Usuario extends REST_Controller
 		$this->load->model('Model_usuario');
     }
     
-   public function list_get()
-   {
-       # code...
-       $dato= $this->Model_usuario->listar_usuario();
-       $this->response($dato);
-   } 
+    public function list_get()
+    {
+        # code...
+        $dato= $this->Model_usuario->listar_usuario();
+        $this->response($dato);
+    } 
 
-  public function guardar_post()
-  {
-      # code...
-    $dato = $this->Model_usuario->guardar_usuario();
-    $this->response($dato);
+    public function guardar_post()
+    {
+        # code...
+        $dato = $this->Model_usuario->guardar_usuario();
+        $this->response($dato);
 
-  }
+    }
+
+    public function loginUsuario_post()
+    {
+        # code...
+        $data = $this->Model_usuario->loginUsuarios();
+                if($data){
+                    $respuesta = array(
+                        'err' => false,
+                        'desc' => "Inicio de sección correcto.",
+                        'usuario' => $data
+                    );
+                }else{
+                    $respuesta = array(
+                        'err' => true,
+                        'desc' => "El usuario o código son incorrectos.",
+                        'usuario' => null
+                    );
+                }
+                $this->response($respuesta);
+    }
+    public function actualizar_put()
+    {
+        # code...
+      $dato = $this->Model_usuario->update_usuario();
+      $this->response($dato);
+    }
    
 }
 
--- application/models/Model_usuario.php
+++ application/models/Model_usuario.php
@@ -5,54 +5,93 @@ date_default_timezone_set('America/La_Paz');
 
 class Model_usuario extends CI_Model
 {
-    public $id;
+    public $idusuario;
+    public $nivel;
     public $nombre;
-    public $paterno;
-    public $materno;
-    public $estado;
-    public $f_registro;
-    public $f_actualizacion;
-    public $intentos;
+    public $tipo_documento;
+    public $num_documento;
+    public $direccion;
+    public $telefono;
+    public $email;
     public $password;
-    public $rol;
-    public $ruta;
+    public $estado;
+    //public $ruta;
+    //public $ci;
+
+
+    public function loginUsuarios()
+    {
+        # code...
+        $this->nombre = $_POST['txtnombre'];
+        $this->password = $_POST['txtpassword'];
+        $this->db->select('*');
+        $array = array('ci' => $this->ci, 'password' => $this->password);
+        $this->db->where($array);
+        $query = $this->db->get('alumnos');
+        return $query->custom_row_object(0,'Model_usuario');
+
+    }
 
     public function guardar_usuario()
     {
         # code...
         $estado_code = array();
-        $registro = new DateTime("now", new DateTimeZone('America/La_Paz'));
-		$registro = $registro->format('Y-m-d');
-        if(isset($_POST["txtnombre"])){
+        /*$registro = new DateTime("now", new DateTimeZone('America/La_Paz'));
+		$registro = $registro->format('Y-m-d');*/
+        if(isset($_POST["txtnombre"]))
+        {
+            $p=$_POST["txtnivel"];
+            $this->nivel = (int)$p;
             $this->nombre = $_POST["txtnombre"];
-            $this->paterno = $_POST["txtpaterno"];
-            $this->materno = $_POST["txtmaterno"];
-            $this->estado = "si";
-            $this->f_registro = $registro;
-            $this->f_actualizacion = $registro;
-            $this->intentos = 0;
-            $this->password = $_POST["txtpass"];
-            $this->rol = 0;
+            $this->tipo_documento = $_POST["txttipo_documento"];
+            $this->num_documento = $_POST["txtnum_documento"];
+            $this->direccion = $_POST["txtdireccion"];
+            $this->telefono = $_POST["txttelefono"];
+            $this->email = $_POST["txtemail"];
+            $this->password = $_POST["txtpassword"];
+            $this->estado = 'activo';
             //para guardar
             $insertado = $this->db->insert('usuario', $this);
-             $estado_code = array("http"=>http_response_code(201),
-                                "estado"=>"ok");
+            $estado_code = array("http"=>http_response_code(201),"estado"=>"ok");
 			//return $this->db->save_queries;
             return $estado_code;
-        }else{
-            return $estado_code = array("http"=>http_response_code(500),
-                                "estado"=>"mall");
+        }
+        else
+        {
+            return $estado_code = array("http"=>http_response_code(500),"estado"=>"mall");
         }
         
     }
     public function listar_usuario()
     {
         # code...
-        $this->db->select('id, nombre, paterno, materno');
+        $this->db->select('idusuario, nombre,direccion,telefono, email');
         $query = $this->db->get('usuario');
         return $query->result();
-    }
-   
+    } 
+    public function update_usuario()
+    {
+        # code...
+        if(isset($_POST["txtidusuario"]))
+        {
+            $this->idusuario = $_POST["txtidusuario"];
+            $this->nombre = $_POST["txtnombre"];
+            //$this->db->update('usuario', array('nombre' => $_POST['txtnombre']), array('idusuario' => $_POST['txtidusuario']));
+            $this->db->where('idusuario', $id_persona);
+            $this->db->update('usuario', array('nombre' => $_POST['txtnombre']), array('idusuario' => $_POST['txtidusuario']));
+             $estado_code = array("http"=>http_response_code(201),"estado"=>"ok","nombre"=>"nombre");
+            return $estado_code;
+        }
+        else
+        {
+            return $estado_code = array("http"=>http_response_code(500),"estado"=>"NO se edito el nombre");
+        }
+
+        public function actualizar($persona, $id_persona) {
+            $this->db->where('id', $id_persona);
+            $this->db->update('personas', $persona);
+        }//end actualizar
+    }   
 }
 
 
