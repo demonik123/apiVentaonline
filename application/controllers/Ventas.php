1521daf03c1f43d99b142b0a993d36f9e7f377b9
Eduk11 <eduardovillegascastillo11@gmail.com>
2022-10-25 21:09:02 -0400

    subi la tarea del listar productos mas vendidos por la fecha

---
+++ application/controllers/Ventas.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Ventas extends REST_Controller
+{
+    function __construct() {
+        parent::__construct();
+		$this->load->database();
+		$this->load->model('Model_ventas');
+    }
+    public function list_get()
+    {
+        # code...
+        $dato= $this->Model_ventas->listar_venta();
+        $this->response($dato);
+    } 
+
+    public function guardar_post()
+    {
+        # code...
+        $dato = $this->Model_ventas->guardar_ventas();
+        $this->response($dato);
+
+    }
+    public function actualizar_post()
+    {
+        # code...
+      $dato = $this->Model_ventas->update_venta();
+      $this->response($dato);
+    }
+    
+    public function buscar_get()
+    {
+        # code...
+      $dato = $this->Model_ventas->buscar_por_fecha();
+      $this->response($dato);
+    }
+}
+?>
