25b31355751199b39f530209a450b2b6afb02eb9
ivanharold001 <ivanharoldbang@gmail.com>
2022-10-27 12:26:38 -0400

    por rango

--- application/controllers/Ventas.php
+++ application/controllers/Ventas.php
@@ -10,38 +10,11 @@ class Ventas extends REST_Controller
 		$this->load->database();
 		$this->load->model('Model_ventas');
     }
-    public function list_get()
-    {
-        # code...
-        $dato= $this->Model_ventas->listar_venta();
-        $this->response($dato);
-    } 
-
-    public function guardar_post()
-    {
-        # code...
-        $dato = $this->Model_ventas->guardar_ventas();
-        $this->response($dato);
-
-    }
-    public function actualizar_post()
-    {
-        # code...
-      $dato = $this->Model_ventas->update_venta();
-      $this->response($dato);
-    }
-    
-    public function buscar_get()
-    {
-        # code...
-      $dato = $this->Model_ventas->buscar_por_fecha();
-      $this->response($dato);
-    }
 
     public function NroRango2_get()
     {
         # code...
-      $dato = $this->Model_ventas->Nro_ventas_por_fecha_rang2();
+      $dato = $this->Model_ventas->Nro_ventas_por_fecha_rango2();
       $this->response($dato);
     }
     
